cf9f39a0d64156ce6e954b224b82647464f4dff3
ArunaLakmal <arunarandunu@gmail.com>
2022-03-21 16:04:02 +0100

    Update awscli

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -5,7 +5,7 @@ sudo apt update
 sudo apt install -y curl unzip sudo git pip
 curl "https://awscli.amazonaws.com/awscli-exe-linux-x86_64.zip" -o "awscliv2.zip"
 unzip awscliv2.zip
-sudo ./aws/install
+sudo ./aws/install --update
 
 curl -fsSL https://deb.nodesource.com/setup_17.x | sudo -E bash -
 sudo apt-get install -y nodejs
